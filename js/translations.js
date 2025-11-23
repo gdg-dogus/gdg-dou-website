9608a29599319d421c3d64fb745f6d6694e75d72
Berat Aydın <berataydiniletisim@gmail.com>
2025-11-24 02:25:03 +0300

    feat: header complete + translate icon

---
+++ js/translations.js
@@ -0,0 +1,18 @@
+export const translations = {
+    tr: {
+        navEvents: "Etkinlikler",
+        navTeams: "Takımlar",
+        navBlog: "Blog",
+        navOffers: "Öğrenci Teklifleri",
+        joinBtn: "GDG'ye Katıl",
+        footer: "© 2025 Google Developer Groups On Campus DOU."
+    },
+    en: {
+        navEvents: "Events",
+        navTeams: "Teams",
+        navBlog: "Blog",
+        navOffers: "Student Offers",
+        joinBtn: "Join GDG",
+        footer: "© 2025 Google Developer Groups On Campus DOU."
+    }
+};
